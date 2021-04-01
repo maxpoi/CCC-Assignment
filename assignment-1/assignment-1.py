4b03df516d1583af24e195fc745fbba5e3ef1825
Jiacheng Ye <jiachengy1@student.unimelb.edu.au>
2021-04-01 21:39:35 +0800

    add mpi support; TODO: change dictionary to numpy array, as it will run even faster

--- assignment-1/assignment-1.py
+++ assignment-1/assignment-1.py
@@ -2,6 +2,10 @@ import json
 import math
 import re
 from collections import defaultdict
+import sys
+import time
+
+from mpi4py import MPI
 
 AFINN = defaultdict(list)
 word_score = []
@@ -39,7 +43,7 @@ f.close()
 # --------- finish reading melbGrid --------- #
 
 
-def getGrid(x, y, grid=melbGrid):
+def get_grid(x, y, grid=melbGrid):
     '''
     x, y, grid: float
 
@@ -131,146 +135,208 @@ def preprocess_word(word):
     return ret
 
 
+def preprocess_tweet(scores, location, tweet):
+    debug = []
 
+    x, y = json.loads(location)["coordinates"]
+    loc = get_grid(x, y)
 
-def getScore(filename, start_row, rank):
+    tweet = json.loads(tweet)["text"]
+    pre_words = tweet.lower().split()
+
+    # preprocessing words
+    # remove " ' at the front
+    # replace " ' , . with ' '
+    words = []
+    for word in pre_words:
+        word = preprocess_word(word)
+        new_list = word.split(" ")
+        for new_word in new_list:
+            if len(new_word) > 1:
+                words.append(new_word)
+
+    len_words = len(words)
+
+    # take each index of words as the starting point
+    # and use max_words to create a slicing window
+    # start, end, last_end are array indexes.
+    start = 0
+    end = max_words
+    while start < len_words:
+
+        # skip obvious mismatches
+        leading_char = words[start][0]
+        if leading_char not in AFINN:
+            start += 1
+            end = start + max_words-1
+
+            continue
+
+        # indicates whether there is a match with current window size
+        found = False
+
+        while end > start:
+            # skip obvious problems
+            while end > len_words:
+                end -= 1
+
+            word = " ".join(words[start:end])
+
+            # loop through AFINN to find a match
+            for target, score in AFINN.get(leading_char):
+                if target == word:
+                    # debug.append(word)
+
+                    scores[loc] += score
+                    found = True
+                    break
+
+                if word < target:
+                    break
+
+            if found:
+                break
+            else:
+                end -= 1
+
+        # deal with situations like 
+        # AFINN: word one; one two
+        # words: word one two
+        # match both
+        # However, note:
+        # if words are: cool nice; and AFINN are: cool nice, nice
+        # then it matches both as well.
+        start += 1
+        end = start + max_words
+
+    return scores, debug
+
+def get_score_individual(filename, rank, num_cores):
     '''
     filename: must be a json file
     '''
     debug = []
 
-    tweets = []
-    locations = []
+    scores = defaultdict(int)
+    # for grid_info in melbGrid:
+    #     scores[grid_info['properties']['id']] = 0
+    # ------- finish initializing ------- #
+
     counter = 0
     with open(filename, "r") as f:
         f.readline()
         for line in f:
-            if counter % rank == 0:
+            if counter % num_cores == rank:
                 loc, twt = extract_field(line)
                 if loc != None and twt != None:
-                    locations.append(json.loads(loc))
-                    tweets.append(json.loads(twt))
+                    # for each instance of tweeter's data (tweet)
+                    # get its text & location
+                    # compare each word in the text with AFINN
+                    # as AFINN is sorted, when word < target_word, stop searching
+                    # otherwise, keep searching until reaches the end of AFINN
+                    scores, debug_inner = preprocess_tweet(scores, loc, twt)
+
+                    debug.append(str(debug_inner))
 
             counter += 1
     f.close()
 
-    scores = {}
-    for grid_info in melbGrid:
-        scores[grid_info['properties']['id']] = 0
-
-    # ------- finish initializing ------- #
-
-    # for each instance of tweeter's data (tweet)
-    # get its text & location
-    # compare each word in the text with AFINN
-    # as AFINN is sorted, when word < target_word, stop searching
-    # otherwise, keep searching until reaches the end of AFINN
-    for row in range(len(tweets)):
-        debug_inner = []
-
-        x, y = locations[row]["coordinates"]
-        pre_words = tweets[row]["text"].lower().split()
-        loc = getGrid(x, y)
-
-        # preprocessing words
-        # remove " ' at the front
-        # replace " ' , . with ' '
-        words = []
-        for word in pre_words:
-            word = preprocess_word(word)
-            new_list = word.split(" ")
-            for new_word in new_list:
-                if len(new_word) > 1:
-                    words.append(new_word)
-
-        len_words = len(words)
-
-        # take each index of words as the starting point
-        # and use max_words to create a slicing window
-        # start, end, last_end are array indexes.
-        start = 0
-        end = max_words
-        while start < len_words:
-
-            # skip obvious mismatches
-            leading_char = words[start][0]
-            if leading_char not in AFINN:
-                start += 1
-                end = start + max_words-1
-
-                continue
-
-            # indicates whether there is a match with current window size
-            found = False
-
-            while end > start:
-                # skip obvious problems
-                while end > len_words:
-                    end -= 1
-
-                word = " ".join(words[start:end])
-
-                # loop through AFINN to find a match
-                for target, score in AFINN.get(leading_char):
-                    if target == word:
-                        # debug_inner.append(word)
-
-                        scores[loc] += score
-                        found = True
-                        break
-
-                    if word < target:
-                        break
-
-                if found:
-                    break
-                else:
-                    end -= 1
-
-            # deal with situations like 
-            # AFINN: word one; one two
-            # words: word one two
-            # match both
-            # However, note:
-            # if words are: cool nice; and AFINN are: cool nice, nice
-            # then it matches both as well.
-            start += 1
-            end = start + max_words
-
-        # debug.append(str(debug_inner))
-
     return scores, debug   
 
-import sys
-import time
+
+def collect_scores(world):
+    size = world.Get_size()
+
+    score_list = []
+
+    # receive and send must in the same order
+    # the tag information is not that useful in our context
+
+    # except master, everyelse is sending data to master
+    # so master (rank_0) needs to send request to all of them
+    for i in range(size-1):
+        world.send("r", dest = (i+1), tag = (i+1))
+    
+    # then receive the returned data
+    for i in range(size-1):
+        score_list.append(world.recv(source=(i+1), tag=0))
+
+    return score_list
+
+
+def worker(world, filename):
+    rank = world.Get_rank()
+    size = world.Get_size()
+
+    scores, debug = get_score_individual(filename, rank, size)
+
+    # wait until data is send
+    while True:
+        in_message = world.recv(source=0, tag=rank)
+        if isinstance(in_message, str):
+            if in_message == "r":
+                world.send(scores, dest=0, tag=0)
+            elif in_message == "e":
+                exit(0)
+
+
+def master(world, filename):
+    rank = world.Get_rank()
+    size = world.Get_size()
+
+    scores, debug = get_score_individual(filename, rank, size)
+    if size > 1:
+        score_list = collect_scores(world)
+        for score in score_list:
+            for k, v in score.items():
+                scores[k] = scores[k] + v
+
+        # close communication
+        for i in range(size-1):
+            world.send("e", dest=(i+1), tag=(i+1))
+
+    return scores
+
+
+
 
 if __name__ == "__main__":
 
-    start_time = time.time()
+    # ---------------- create MPI world ----------------
+    world = MPI.COMM_WORLD
+    rank = world.Get_rank()
 
-    print("------------      log       -------------")
+    if rank == 0:
+        start_time = time.time()
+        print("------------      log       -------------")
 
+    # --------------- read argv -------------------
     try:
         filename = str(sys.argv[1])
     except:
         print("Please provide a json file...")
         sys.exit(1)
 
-    if (len(sys.argv) > 2):
-        n_nodes = int(sys.argv[2])
-        n_cores = int(sys.argv[3])
+    # if (len(sys.argv) > 2):
+    #     n_nodes = int(sys.argv[2])
+    #     n_cores = int(sys.argv[3])
+    # else:
+    #     n_nodes = 1
+    #     n_cores = 1
+
+    print("name: {}, rank: {}, size: {}".format(filename, rank, world.Get_size()))
+
+    # initiate via rank
+    if rank == 0:
+        scores = master(world, filename)
     else:
-        n_nodes = 1
-        n_cores = 1
-
-    print("name: {}, nodes: {}, cores: {}".format(filename, n_nodes, n_cores))
-
-    score, debug = getScore(filename, 0)
-    print(score) 
-    print("running time: %s " % (time.time() - start_time))
+        worker(world, filename)
+    
+    if rank == 0:
+        print(scores)
+        print("running time: %s " % (time.time() - start_time))
+        print("------------ finish logging -------------")
 
     # with open("debug-" + filename[:-5] + ".txt", "w") as f:
     #     for instance in debug:
     #         f.write(instance + "\n")
-
-    print("------------ finish logging -------------")
